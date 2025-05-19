48dcb0fe3a105b5d4db6d0b86be9159b6f97d829
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-05-19 23:00:38 +0900

    何もわからん

--- co2.py
+++ co2.py
@@ -1,8 +1,19 @@
 import mh_z19
+import time 
 
 def read():
     out = mh_z19.read()
     return str(out)[8:-1]
+
+def read_all():
+    out = mh_z19.read_all()
+    return str(out)[8:-1]
 if __name__=='__main__':
-    co2=read()
-    print(co2)
+    while True : 
+
+      co2=read()
+      co2_all=read_all()
+      print(co2)
+      print(co2_all)
+      time.sleep(2)
+
